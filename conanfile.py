56369df3685f8396be41f1f12ca8ad54e74efaec
azriel <azriel91@gmail.com>
2015-12-29 15:31:26 +1300

    Updated version to 0.2.0

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import *
 
 class ConanCmakeConan(ConanFile):
     name = 'conan_cmake'
-    version = '0.1.0'
+    version = '0.2.0'
     exports = ['conanTools.cmake']
 
     def package(self):
